0be88f29571dc64229f4b0e9ef4eb5e4ad4daf3f
Peiyi <peiyic03@gmail.com>
2023-01-11 11:24:17 -0500

    change coeeficient of variant in run all file

--- run_all.sh
+++ run_all.sh
@@ -3,5 +3,5 @@ for L in {1..5}; do
   echo "====================================="
   echo "running L $L sample $sample"
   echo "====================================="
-  python generate_random_field.py 25 4000 250 $L
+  python generate_random_field.py 64 4000 1440000 $L
 done
