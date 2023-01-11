0399c395ce5d7c5533a33236e3a3ea30a3fa9575
Peiyi <peiyic03@gmail.com>
2023-01-11 11:32:13 -0500

    update run all so all cores are used

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
-for L in {1..5}; do
-  echo "====================================="
-  echo "running L $L sample $sample"
-  echo "====================================="
-  python generate_random_field.py 64 4000 1440000 $L
+count = 0
+for L in {0..999}; do
+  python generate_random_field.py 64 4000 1440000 $L &
+  (( count ++ ))
+  if (( count = 10 )); then
+    wait
+    count=0
+  fi
 done
+
+
+
+
+# for L in {1..5}; do
+#   echo "====================================="
+#   echo "running L $L sample $sample"
+#   echo "====================================="
+#   python generate_random_field.py 64 4000 1440000 $L
+# done
